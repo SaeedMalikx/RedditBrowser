c59c7396f4f88ff1acffd83ab039b983190d5946
saeedmalikx <saeedmalik360@gmail.com>
2017-07-11 17:50:53 -0400

    menu, userposts and new expander

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import axios from 'axios';
 
 
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import Drawer from 'material-ui/Drawer';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import Upvote from 'material-ui/svg-icons/navigation/arrow-upward';
 import Comments from 'material-ui/svg-icons/communication/comment';
+import Author from 'material-ui/svg-icons/social/person';
 
 
 class App extends Component {
@@ -16,15 +20,25 @@ class App extends Component {
     this.state = {
       posts: [],
       comments: [],
-      subposts: []
+      subposts: [],
+      subreddits: ["all", "soccer", "askreddit", "videos"],
+      subopen: false
     };
   }
 
   componentDidMount(){
     this.getposts()
   }
+  openmenu = () => this.setState({subopen: !this.state.subopen})
   getposts = () => {
     axios.get('https://www.reddit.com/r/all/hot.json?limit=25')
+      .then(res => {
+        this.setState({ posts: res.data.data.children });
+      })
+  }
+  getpostsmenu = (sub) => {
+    this.setState({subopen: false})
+    axios.get("https://www.reddit.com/r/"+sub+"/hot.json?limit=25")
       .then(res => {
         this.setState({ posts: res.data.data.children });
       })
@@ -36,33 +50,44 @@ class App extends Component {
       })
   }
   getcomments = (sub, post) => {
+    this.setState({comments: []})
     axios.get("https://www.reddit.com/r/"+ sub + "/comments/"+ post +".json")
       .then(res => {
         this.setState({ comments: res.data[1].data.children });
       })
   }
+  getuser = (user) => {
+    this.setState({posts: []})
+    axios.get("https://www.reddit.com/user/"+ user + ".json?sort=top")
+      .then(res => {
+        this.setState({ posts: res.data.data.children });
+      })
+  }
   render() {
     return (
       <div >
+        <AppBar title="RedditLite" onLeftIconButtonTouchTap={this.openmenu}/>
+        <Drawer open={this.state.subopen} >
+          {this.state.subreddits.map((sub, index)=> <MenuItem onClick={()=>{this.getpostsmenu(sub)}}>{sub}</MenuItem>)}
+        </Drawer>
         { this.state.posts.map((post, index) =>
-          <Card key={index} onClick={() => {this.getcomments(post.data.subreddit, post.data.id)}}>
+          <Card key={index} onExpandChange={() => {this.getcomments(post.data.subreddit, post.data.id)}} >
             <CardHeader
               title={post.data.title}
               avatar={post.data.thumbnail}
-              actAsExpander={true}
-              showExpandableButton={true}
             />
-            <CardActions>
+            
               <FlatButton label={post.data.score}  disabled={true} icon={<Upvote/>}/>
-              <FlatButton label={post.data.num_comments}  disabled={true} icon={<Comments/>}/>
+              <FlatButton label={post.data.num_comments} actAsExpander={true}  icon={<Comments/>} onClick={() => {this.getcomments(post.data.subreddit, post.data.id)}}/>
               <FlatButton label={post.data.subreddit} onClick={()=>{this.getsubposts(post.data.subreddit)}}/>
-            </CardActions>
+              <FlatButton label={post.data.author}  onClick={()=>{this.getuser(post.data.author)}} icon={<Author/>}/>
+            
             <CardText expandable={true}>
               <div className="commentscroll"> 
                 {this.state.comments.map((comment, index) =>
                   
-                    <p>{comment.data.body}</p>
-                  
+                    <p key={index}>{comment.data.body}</p>
+                      
                 )}
               </div>
             </CardText>
